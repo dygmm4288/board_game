391338477dcc6d552fef4dc6bc1abff9e9068e0d
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-07-23 21:37:21 +0900

    update: user model에 id값 추가

--- server/app/models.py
+++ server/app/models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from .database import Base
 
 class User(Base) :
   __tablename__ = 'users'
-  
-  username = Column(String, primary_key=True, index=True)
+
+  id = Column(Integer, primary_key=True, index=True)  
+  username = Column(String,nullable=False, index=True)
   password = Column(String, nullable=False)
