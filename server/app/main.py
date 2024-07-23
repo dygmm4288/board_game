62e7954f5c4b41e5c33b083d731b5bb4d1afaabe
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-07-23 22:05:15 +0900

    add: user router등록 및 회원가입 api 등록

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from typing import List
+from dotenv import load_dotenv
+from router import auth
 
 app = FastAPI()
 
+app.include_router(auth.router)
+
 if __name__ == '__main__' :
   import uvicorn
+  load_dotenv('../portal.env')
   uvicorn.run(app, host="127.0.0.1", port=8080, log_level='debug')
