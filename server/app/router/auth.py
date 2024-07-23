04c9216f17c74233e33a411123e480d3f4739230
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-07-23 22:04:01 +0900

    add: user router등록 및 회원가입 api 등록

---
+++ server/app/router/auth.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from os import environ
+from schemas import UserCreate
+from user_crud import create_user
+from database import get_db
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+SECRET_KEY = environ.get('SECRET_KEY', None) 
+ALGORITHM = "HS256"
+
+router = APIRouter(prefix='/api/auth')
+
+@router.post('/')
+async def login() :
+  pass
+
+@router.post('/regist')
+def regist_user(_user_create: UserCreate, db:AsyncSession=Depends(get_db)) :
+  create_user(db=db, user_create=_user_create)
