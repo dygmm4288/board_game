d01458628a1997ba1d8d2dc9092b7d58c1dd80b2
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-07-23 22:21:09 +0900

    [add] user_crud : get user 내용추가

--- server/app/user_crud.py
+++ server/app/user_crud.py
@@ -1,13 +1,19 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from schemas import UserCreate, User
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=['bcrypt'])
 
-def create_user(user_create:UserCreate, db:AsyncSession):
+async def create_user(user_create:UserCreate, db:AsyncSession):
   user = User(
     username=user_create.username,
     password=pwd_context.has(user_create.password),
   )
   db.add(user)
   return user
+
+async def get_user(db:AsyncSession, id: int) :
+  result = await db.execute(select(User).filter(User.id == id))
+  return result.scalar_one()
+  
