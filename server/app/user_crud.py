a791ae1293820ee0bc9e03adf9a9f552e8299479
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-07-23 21:59:11 +0900

    update: 매개변수 수정

--- server/app/user_crud.py
+++ server/app/user_crud.py
@@ -1,12 +1,10 @@
-from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from schemas import UserCreate, User
-from database import get_db
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=['bcrypt'])
 
-def create_user(user_create:UserCreate, db:AsyncSession = Depends(get_db)):
+def create_user(user_create:UserCreate, db:AsyncSession):
   user = User(
     username=user_create.username,
     password=pwd_context.has(user_create.password),
