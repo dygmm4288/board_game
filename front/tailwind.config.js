cf6c5d2f3ed4920267db86f3b87332d7cf46f793
mirae-Jo <mirae9873@gmail.com>
2024-07-29 21:48:56 +0900

    tailwind css 컨피그 설정 postcss, autoprefixer 설치 FirstVisit 페이지 생성

---
+++ front/tailwind.config.js
@@ -0,0 +1,11 @@
+const config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      ["primary-color"]: "var(--primary-color)",
+      ["primary-background-color"]: "var(--primary-background-color)",
+    },
+  },
+  plugins: [],
+};
+export default config;
