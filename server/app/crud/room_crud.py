da08508ef903b45f04b4f227f9e5861f905f1819
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-07-30 23:32:34 +0900

    [update] room crud , room router 로직 추가

--- server/app/crud/room_crud.py
+++ server/app/crud/room_crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 from models import Room
 import uuid
 
@@ -13,9 +14,30 @@ def create_room(max_players:int, db:Session) :
 
   return room
 
-def put_room(confirm:str, db:Session) :
+def put_room(confirm:str, _room:Room, db:Session) :
+  
+  if confirm == '게임시작' :
+    
+    if _room.status != 'waiting' :
+      raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="잘못된 접근입니다"
+      )
+    
+    _room.status = 'in-progress'
 
-  pass
+  elif confirm == '게임종료' :
+    
+    if _room.status != 'in-progress' :
+      raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="잘못된 접근입니다"
+      )
+
+    _room.status = 'waiting'
+    
+  return _room
+    
 
 def get_room(r_id:int, db:Session) :
   result = db.query(Room).filter(Room.id == r_id).first()
