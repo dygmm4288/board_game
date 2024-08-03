fdc13b557447d1b536948c5afff17e2a4cccafe4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-03 20:04:52 +0900

    [add] Select component 추가

---
+++ front/src/components/form/Select.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+type Props = {
+  options: string[];
+  setSelectValue: (option: string) => void;
+};
+const Select = ({ options, setSelectValue }: Props) => {
+  const [selectedOption, setOption] = useState(options[0]);
+
+  const handleSelectOption = (option: string) => () => {
+    if (selectedOption === option) return;
+    setOption(option);
+  };
+
+  useEffect(() => {
+    setSelectValue(selectedOption);
+  }, [selectedOption]);
+
+  return (
+    <select>
+      {options.map((option) => (
+        <option
+          key={`options-${option}`}
+          selected={selectedOption === selectedOption}
+          onClick={handleSelectOption(option)}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
