18b7c8fb1331802ed070c8746a4ede63e4bd1646
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-04 17:20:12 +0900

    [update] room modal: useRoom 훅 적용

--- front/src/components/common/modal/RoomCreateModal.tsx
+++ front/src/components/common/modal/RoomCreateModal.tsx
@@ -1,8 +1,17 @@
 import { flex_center } from "../../../css/flex";
+import useRoom from "../../../hooks/useRoom";
 import { cn } from "../../../util/cn";
 import NumberInput from "../../form/NumberInput";
 
 const RoomModal = () => {
+  const {createRoom} = useRoom();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createRoom()
+  };
+  
   return (
     <article className='px-[50px] py-[20px] w-[240px] flex flex-col gap-[20px] bg-white rounded-10'>
       <h1 className='text-center title'>방 만들기</h1>
@@ -11,7 +20,7 @@ const RoomModal = () => {
           <option>Minibill2</option>
         </select>
         <NumberInput />
-        <button className={cn("w-full h-[40px] bg-secondary-bg-color rounded-[5px]", flex_center)}>만들기</button>
+        <button type="submit" className={cn("w-full h-[40px] bg-secondary-bg-color rounded-[5px]", flex_center)}>만들기</button>
       </form>
     </article>
   );
