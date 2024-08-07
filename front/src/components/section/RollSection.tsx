b4bbb24e1e94c9d347490fe30c4f335ac01bec6e
mirae-Jo <mirae9873@gmail.com>
2024-08-07 19:27:16 +0900

    주사위 난수 생성 완

--- front/src/components/section/RollSection.tsx
+++ front/src/components/section/RollSection.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 import { roll_flex } from "../../css/flex_roll";
-import { border_roll } from "../../css/roll_border";
+import { roll_style } from "../../css/roll_style";
 import { cn } from "../../util/cn";
 
 const RollSection = () => {
   const [firstNum, setFirstNum] = useState<number | undefined>();
+  const [secondNum, setSecondNum] = useState<number | undefined>();
+  const [thirdNum, setThirdNum] = useState<number | undefined>();
 
   const handleFirstRoll = () => {
-    return setFirstNum(Math.random());
+    const randomFloor = Math.floor(Math.random() * 6 + 1);
+
+    return setFirstNum(randomFloor);
+  };
+
+  const handleSecondRoll = () => {
+    const randomFloor = Math.floor(Math.random() * 6 + 1);
+    const randomFloor2 = Math.floor(Math.random() * 6 + 1);
+
+    return setSecondNum(randomFloor), setThirdNum(randomFloor2);
   };
 
   return (
     <section className='p-[10px]'>
-      <div className='flex flex-row justify-center gap-[40px] text-primary-font-color text-[12px]'>
-        <div onClick={handleFirstRoll} className={cn(roll_flex)}>
+      <div className='flex flex-row items-center justify-center gap-[40px] text-primary-font-color text-[12px]'>
+        <div
+          onClick={handleFirstRoll}
+          className={cn(roll_flex) + " hover:cursor-pointer"}>
           <p>주사위 1</p>
-          <div className={cn(border_roll)}>{firstNum}</div>
+          <div className={cn(roll_style)}>{firstNum}</div>
         </div>
-        <div className={cn(roll_flex)}>
+        <div
+          onClick={handleSecondRoll}
+          className={cn(roll_flex) + " hover:cursor-pointer"}>
           <p>주사위 2</p>
           <div className='flex flex-row gap-[10px]'>
-            <div className={cn(border_roll)}></div>
-            <div className={cn(border_roll)}></div>
+            <div className={cn(roll_style)}>{secondNum}</div>
+            <div className={cn(roll_style)}>{thirdNum}</div>
           </div>
         </div>
       </div>
--- front/src/css/roll_border.ts
+++
@@ -1,3 +0,0 @@
-export const border_roll = () => {
-  return "w-[30px] h-[30px] border border-primary-border-color border-solid rounded-[5px]";
-};
