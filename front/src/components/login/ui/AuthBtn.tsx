daa6f1eab6b9bf3ed21d4cb009cc760a52e2060c
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-07 23:25:46 +0900

    (update) 공통으로 빼냄

--- front/src/components/login/ui/AuthBtn.tsx
+++ front/src/components/login/ui/AuthBtn.tsx
@@ -1,8 +1,13 @@
+const Btn = (type: "로그인" | "회원가입") => (
+  <button className='p-5 bg-indigo-400 rounded-lg' type='submit'>
+    {type}
+  </button>
+);
 
-const LoginBtn = () => <button type='submit'>login</button>
-const SignUpBtn = () => <button type='submit'>signup</button>
+const LoginBtn = () => Btn("로그인");
+const SignUpBtn = () => Btn("회원가입");
 
-const AuthBtn = ({isLoginMode}:{isLoginMode: boolean}) => isLoginMode ? <LoginBtn /> : <SignUpBtn/>
-
+const AuthBtn = ({ isLoginMode }: { isLoginMode: boolean }) =>
+  isLoginMode ? <LoginBtn /> : <SignUpBtn />;
 
 export default AuthBtn;
