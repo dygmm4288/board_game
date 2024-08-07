df36699371f377a4f8233a770644b8fd0873bffc
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-07 23:24:16 +0900

    (update) 라벨 추가 및 css 추가

--- front/src/components/login/ui/Input.tsx
+++ front/src/components/login/ui/Input.tsx
@@ -1,19 +1,30 @@
 import { ChangeEvent, InputHTMLAttributes, useState } from "react";
 
 interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  onChange: (e:ChangeEvent<HTMLInputElement>) => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  label?: string;
 }
 
-const Input = ({onChange, ...inputProps}: CustomInputProps) => {
-  const [value,setValue] = useState('');
-  
-  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+const Input = ({ onChange, label, ...inputProps }: CustomInputProps) => {
+  const [value, setValue] = useState("");
+  const { id } = inputProps;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
     onChange(e);
   };
 
-  return <input {...inputProps} value={value} onChange={handleChange}/>;
+  return (
+    <div className='flex flex-col gap-2 w-full'>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        {...inputProps}
+        className='border border-blue-100 rounded-lg p-2'
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
-
 
 export default Input;
