0ae0d1ffd594b7a51730b0f47980977d48ba9169
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-07 23:25:16 +0900

    (update) 라벨 적용 및 css 적용

--- front/src/components/login/LoginForm.tsx
+++ front/src/components/login/LoginForm.tsx
@@ -3,15 +3,33 @@ import AuthBtn from "./ui/AuthBtn";
 import Input from "./ui/Input";
 
 const LoginForm = () => {
-  const {handleSubmit, handleChangeValue, isLoginMode} = useLogin();  
+  const { handleSubmit, handleChangeValue, isLoginMode } = useLogin();
 
-
-  return <form onSubmit={handleSubmit}>
-    <Input type="text" id="username" onChange={handleChangeValue('username')}/>
-    <Input type="password" id="password" onChange={handleChangeValue('password')}/>
-    {!isLoginMode && <Input id="password-confirm" type='password'  onChange={handleChangeValue('passwordConfirm')}/>}
-    <AuthBtn isLoginMode={isLoginMode}/>
-  </form>
+  return (
+    <form className='flex flex-col gap-4 p-2' onSubmit={handleSubmit}>
+      <Input
+        type='text'
+        id='username'
+        label='유저 이름'
+        onChange={handleChangeValue("username")}
+      />
+      <Input
+        type='password'
+        id='password'
+        label='비밀번호'
+        onChange={handleChangeValue("password")}
+      />
+      {!isLoginMode && (
+        <Input
+          id='password-confirm'
+          label='비밀번호 확인'
+          type='password'
+          onChange={handleChangeValue("passwordConfirm")}
+        />
+      )}
+      <AuthBtn isLoginMode={isLoginMode} />
+    </form>
+  );
 };
 
 export default LoginForm;
