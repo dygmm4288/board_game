1ee4a25000f5666db6da5d81b14b0f8c9a5db74e
mirae-Jo <mirae9873@gmail.com>
2024-08-09 19:58:34 +0900

    router 수정

--- front/src/router/index.tsx
+++ front/src/router/index.tsx
@@ -2,20 +2,25 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
+  Routes,
 } from "react-router-dom";
 
 import Login from "../components/routes/Login";
 import Signup from "../components/routes/Signup";
 import Home from "../components/Home";
 import HomeLayout from "../layouts/HomeLayout";
+import MinivilleRoom from "../components/routes/MinivilleRoom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<HomeLayout />}>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-    </Route>
+    <Routes>
+      <Route path='/' element={<HomeLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Route>
+      <Route path='/miniville_room' element={<MinivilleRoom />}></Route>
+    </Routes>
   )
 );
 
