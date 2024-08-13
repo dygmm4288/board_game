d27d1726d98709900d82dabe4ad055bc52609b14
mirae-Jo <mirae9873@gmail.com>
2024-08-13 21:24:04 +0900

    gamelog section 컴포넌트 수정

--- front/src/components/section/GameLogSection.tsx
+++ front/src/components/section/GameLogSection.tsx
@@ -1,8 +1,22 @@
-import React from "react";
-
 const GameLogSection = () => {
+  const players = [{ name: "a" }, { name: "b" }];
   return (
-    <div className='absolute bottom-0 w-full h-[130px] bg-black/20 border border-solid border-primary-font-color'></div>
+    <div className='absolute bottom-0 w-full h-[130px] bg-black/20 border border-solid border-primary-font-color py-[10px] px-[20px] text-white'>
+      {players.map((player) => {
+        return (
+          <div className='flex flex-row gap-[4px]'>
+            <div>{player.name} :</div>
+            <div className='flex flex-col text-wrap'>
+              <p>log</p>
+              <p>
+                manymanylogs manymanylogs manymanylogs manymanylogs manymanylogs
+                manymanylogs manymanylogs
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
