f6d53c742e4e2dd531f4d794db2d57af58916ebd
mirae-Jo <mirae9873@gmail.com>
2024-08-15 15:32:37 +0900

    로그아웃 ui & 기능 추가

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HomeIcon from "../assets/svg/HomeIcon";
+import LogoutIcon from "../assets/svg/LogoutIcon";
+import useAuth from "../zustand/auth";
 
 const Header = () => {
+  const { logout } = useAuth();
+  const handleLogoutBtn = () => {
+    logout();
+  };
   return (
-    <nav className='w-full h-[60px] flex items-center justify-center shadow-header-nav sticky z-20'>
-      <li>
+    <nav className='relative w-full h-[60px] shadow-header-nav sticky z-20 text-primary-font-color text-10'>
+      <li className='absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]'>
         <Link to={"/"} className='flex flex-col items-center justify-center'>
           <HomeIcon width={28} height={28} />
-          <h1 className=' text-primary-font-color text-10'>홈</h1>
+          <h1>홈</h1>
         </Link>
+      </li>
+      <li className='absolute right-[22px] top-[50%] translate-y-[-50%]'>
+        <button onClick={handleLogoutBtn}>
+          <LogoutIcon />
+          <h2>로그아웃</h2>
+        </button>
       </li>
     </nav>
   );
