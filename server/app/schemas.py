10ad3cfc21ab7bc30ace8d97a6de4e386efa2a46
mirae-Jo <mirae9873@gmail.com>
2024-08-18 20:00:01 +0900

    pulling

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -44,15 +44,12 @@ class Room(BaseModel) :
   id:str
   status: str
   max_players: int
-<<<<<<< HEAD
   game_name:str
   created_by:str
 
   class Config:
     orm_mode = True
-=======
   game : Union[int, Game] = None
->>>>>>> b04d5164a55f9b84e7b6c50df3852f9a48a247b0
 
   @field_validator('max_players')
   def validate_max_players(cls, v) :
