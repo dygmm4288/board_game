ebe20cbdecd0a874ab3ac6b1590a04ae9dc36dbb
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-25 23:08:22 +0900

    (update) room react query 적용, 인증 관리

--- front/src/hooks/useRoom.ts
+++ front/src/hooks/useRoom.ts
@@ -1,21 +1,54 @@
-import { Room, createRoom, deleteRoom, getRooms } from "../api/room";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useEffect } from "react";
+import roomApi from "../api/room";
+import { getErrorMsg } from "../util/error";
+import useAuth from "../zustand/auth";
+
+const GET_ROOM = "room/get";
 
 const useRoom = () => {
-  const create = (max_players: number, game: string) => {
-    return createRoom(max_players, game);
+  const { logout } = useAuth();
+
+  const onError = (err: AxiosError) => {
+    const msg = getErrorMsg(err);
+
+    if (msg === "inactive user") logout();
   };
 
-  const get = (): Promise<Room[]> => {
-    return getRooms();
-  };
+  const { data: rooms, error: getRoomsError } = useQuery({
+    queryKey: [GET_ROOM],
+    queryFn: () => roomApi.getRooms(),
+  });
 
-  const remove = (roomId: number) => {
-    return deleteRoom(roomId);
-  };
+  const { mutateAsync: create } = useMutation({
+    mutationFn: (data: { max_players: number; game: string }) =>
+      roomApi.createRoom(data.max_players, data.game),
+    onError,
+    retry: false,
+  });
+
+  const { mutate: remove } = useMutation({
+    mutationFn: (roomId: number) => roomApi.deleteRoom(roomId),
+    onError,
+    retry: false,
+  });
+
+  const { mutateAsync: put } = useMutation({
+    mutationFn: (data: {
+      id: number;
+      body: { confirm: string; updates: Record<string, string> };
+    }) => roomApi.putRoom(data.id, data.body),
+  });
+
+  useEffect(() => {
+    if (getRoomsError) onError(getRoomsError as AxiosError);
+  }, [getRoomsError]);
 
   return {
     create,
-    get,
+    put,
+    rooms,
     remove,
   };
 };
