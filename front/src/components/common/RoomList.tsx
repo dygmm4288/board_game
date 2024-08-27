94e07ba0504cbe3d7bff2b5f9e16cc907f4c6ba4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-27 20:36:34 +0900

    (update) useLoading hook화

--- front/src/components/common/RoomList.tsx
+++ front/src/components/common/RoomList.tsx
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
 import PersonIcon from "../../assets/svg/PersonIcon";
 import RollIcon from "../../assets/svg/RollIcon";
+import useLoading from "../../hooks/useLoading";
 import useRoom from "../../hooks/useRoom";
-import useModal from "../../zustand/modal";
-import LoadingSpinner from "./modal/LoadingSpinner";
 
 const RoomList = () => {
   const { rooms, roomsIsPending, remove } = useRoom();
-  const { show, hide } = useModal();
+
+  useLoading({ isShow: roomsIsPending });
 
   const handleDelete = async (id: number) => {
     try {
@@ -16,14 +15,6 @@ const RoomList = () => {
       console.error("fail", error);
     }
   };
-
-  useEffect(() => {
-    if (roomsIsPending) {
-      show({ component: <LoadingSpinner /> });
-      return;
-    }
-    hide();
-  }, [roomsIsPending]);
 
   return (
     <ul className='w-[315px] flex flex-wrap justify-between gap-y-[20px]'>
