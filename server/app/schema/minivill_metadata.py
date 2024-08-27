1f722d0a65ea97c5a6f74b33383f3d156656d8ce
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-27 20:47:59 +0900

    (update) minibill -> minivill 수정

--- server/app/schema/minivill_metadata.py
+++ server/app/schema/minivill_metadata.py
@@ -1,5 +1,5 @@
 from typing import List
-from schema.minibill import Establishment, LandMark
+from server.app.schema.minivill import Establishment, LandMark
 
 establishments = [
     Establishment(
