b5a866b0e988981051bc11acf6aa3ff77ae37b71
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-08-28 23:51:33 +0900

    (refactor) rollsection refactor

--- front/src/components/section/RollSection.tsx
+++ front/src/components/section/RollSection.tsx
@@ -3,22 +3,23 @@ import { roll_flex } from "../../css/flex_roll";
 import { roll_style } from "../../css/roll_style";
 import { cn } from "../../util/cn";
 
+const getRandomRoll = () => Math.floor(Math.random() * 6 + 1);
+const setRoll = (
+  setter: React.Dispatch<React.SetStateAction<number | undefined>>,
+) => setter(getRandomRoll());
+
 const RollSection = () => {
   const [firstNum, setFirstNum] = useState<number | undefined>();
   const [secondNum, setSecondNum] = useState<number | undefined>();
   const [thirdNum, setThirdNum] = useState<number | undefined>();
 
   const handleFirstRoll = () => {
-    const randomFloor = Math.floor(Math.random() * 6 + 1);
-
-    return setFirstNum(randomFloor);
+    setRoll(setFirstNum);
   };
 
   const handleSecondRoll = () => {
-    const randomFloor = Math.floor(Math.random() * 6 + 1);
-    const randomFloor2 = Math.floor(Math.random() * 6 + 1);
-
-    return setSecondNum(randomFloor), setThirdNum(randomFloor2);
+    setRoll(setSecondNum);
+    setRoll(setThirdNum);
   };
 
   return (
@@ -26,13 +27,13 @@ const RollSection = () => {
       <div className='flex flex-row items-center justify-center gap-[40px] text-primary-font-color text-[12px]'>
         <div
           onClick={handleFirstRoll}
-          className={cn(roll_flex) + " hover:cursor-pointer"}>
+          className={cn(roll_flex, "hover:cursor-pointer")}>
           <p>주사위 1</p>
           <div className={cn(roll_style)}>{firstNum}</div>
         </div>
         <div
           onClick={handleSecondRoll}
-          className={cn(roll_flex) + " hover:cursor-pointer"}>
+          className={cn(roll_flex, "hover:cursor-pointer")}>
           <p>주사위 2</p>
           <div className='flex flex-row gap-[10px]'>
             <div className={cn(roll_style)}>{secondNum}</div>
