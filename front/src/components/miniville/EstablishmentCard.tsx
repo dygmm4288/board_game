8348803d747ef58585fd5dfeda0cfe74494a0c1a
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-14 20:56:06 +0900

    (add) Card Description 이벤트 추가(mouse enter, mouse leave, touch start, touch end)

--- front/src/components/miniville/EstablishmentCard.tsx
+++ front/src/components/miniville/EstablishmentCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { card_roll } from "../../css/card_roll";
 import { _ESTABLISHMENTS } from "../../miniville_meta_data/establishments/metadata";
 import { EstColor } from "../../miniville_meta_data/establishments/types";
@@ -9,15 +10,14 @@ interface Card {
 
 const EstablishmentCard = ({ id }: Card) => {
   const card = _ESTABLISHMENTS.find((item) => String(item._id) === String(id));
+  const [isShowDescription, setShowDescription] = useState(false);
 
-  const bgColor = (color: EstColor) => {
-    if (color === EstColor.Blue) {
-      return "bg-[#D1E0F5]";
-    } else if (color === EstColor.Red) {
-      return "bg-[#F5D1D1]";
-    } else if (color === EstColor.Green) {
-      return "bg-[#C8E9C8]";
-    } else return "bg-[#E6D1F5]";
+  const handleHover = () => {
+    setShowDescription(true);
+  };
+
+  const handleHoverOut = () => {
+    setShowDescription(false);
   };
 
   if (!card) return <div>카드정보없음</div>;
@@ -27,7 +27,11 @@ const EstablishmentCard = ({ id }: Card) => {
       className={cn(
         "relative w-[54px] h-[72px] p-[4px] text-center",
         bgColor(card.color),
-      )}>
+      )}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHoverOut}
+      onTouchStart={handleHover}
+      onTouchEnd={handleHoverOut}>
       <div
         className={
           "text-[12px] flex flex-col items-center " +
@@ -41,6 +45,7 @@ const EstablishmentCard = ({ id }: Card) => {
       <p className='absolute bottom-[2px] left-[50%] translate-x-[-50%] text-10 text-[#808080]'>
         {card.cost}$
       </p>
+      {isShowDescription && <div>{card.description}</div>}
     </div>
   );
 };
@@ -53,6 +58,16 @@ const RollInfo = ({ rolls }: { rolls: number[] }) => {
       ))}
     </div>
   );
+};
+
+const bgColor = (color: EstColor) => {
+  if (color === EstColor.Blue) {
+    return "bg-[#D1E0F5]";
+  } else if (color === EstColor.Red) {
+    return "bg-[#F5D1D1]";
+  } else if (color === EstColor.Green) {
+    return "bg-[#C8E9C8]";
+  } else return "bg-[#E6D1F5]";
 };
 
 export default EstablishmentCard;
