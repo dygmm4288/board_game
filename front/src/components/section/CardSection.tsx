be825fe4e8867191352d2aefd336a01372307d5b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-14 20:45:40 +0900

    (add) 백에서 받은 card 정보 프론트 디스플레이

--- front/src/components/section/CardSection.tsx
+++ front/src/components/section/CardSection.tsx
@@ -1,11 +1,20 @@
 import useMiniville from "../../zustand/miniville";
+import EstablishmentCard from "../miniville/EstablishmentCard";
 
 const CardSection = () => {
   const { field_16, field_712 } = useMiniville();
-  console.log(field_16, field_712);
+
+  const EstablishmentCards = (cards: { [key: number]: number }) => {
+    return Object.keys(cards).map((id) => (
+      <EstablishmentCard key={`establish-card-${id}`} id={id} />
+    ));
+  };
 
   return (
-    <section className='grid grid-cols-5 justify-items-center gap-[12px] pt-[10px] px-[21px]'></section>
+    <section className='grid grid-cols-5 justify-items-center gap-[12px] pt-[10px] px-[21px]'>
+      {EstablishmentCards(field_16)}
+      {EstablishmentCards(field_712)}
+    </section>
   );
 };
 
