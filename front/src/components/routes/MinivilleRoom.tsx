1b79939d0b999376e72c4afc1a9d842ee16d14d3
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-22 21:49:14 +0900

    (update) alert modal 적용

--- front/src/components/routes/MinivilleRoom.tsx
+++ front/src/components/routes/MinivilleRoom.tsx
@@ -2,6 +2,9 @@ import _ from "lodash";
 import { useParams } from "react-router-dom";
 import useGameJson from "../../hooks/useGameStatus";
 import useRoom from "../../hooks/useRoom";
+import { getErrorMsg } from "../../util/error";
+import useModal from "../../zustand/modal";
+import AlertModal from "../common/modal/AlertModal";
 import PlayerHeader from "../common/PlayerHeader";
 import CardSection from "../section/CardSection";
 import GameLogSection from "../section/GameLogSection";
@@ -9,19 +12,25 @@ import RollSection from "../section/RollSection";
 
 const MinivilleRoom = () => {
   const params = useParams();
-  const id = _.toNumber(_.pick(params, "id"));
+  const id = _.toNumber(_.get(params, "id"));
   useGameJson(id);
-  const { put, is_show_start_btn } = useRoom({
+  const { put /* is_show_start_btn */ } = useRoom({
     id,
   });
+  const { show } = useModal();
 
   const handleStartGame = () => {
+    console.log(id);
     if (!id) {
       console.error("id가 없습니다.");
       return;
     }
 
-    put({ id, body: { confirm: "게임시작" } });
+    put({ id, body: { confirm: "게임시작" } }).catch((err) => {
+      show({
+        component: <AlertModal title={"실패"} content={getErrorMsg(err)} />,
+      });
+    });
   };
 
   return (
@@ -29,7 +38,7 @@ const MinivilleRoom = () => {
       <PlayerHeader />
       <RollSection />
       <CardSection />
-      {is_show_start_btn && (
+      {
         <section className='w-full h-[100px] relative my-10'>
           <button
             className='w-[100px] h-[43px] bg-primary-color text-white rounded-[5px] absolute right-[8px] bottom-0'
@@ -37,7 +46,7 @@ const MinivilleRoom = () => {
             게임시작
           </button>
         </section>
-      )}
+      }
       <GameLogSection />
     </div>
   );
