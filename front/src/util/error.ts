fd29025ea3eac75518c93a187254be2a40e6875a
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-22 21:48:49 +0900

    (update) error 함수 수정
    1. getErrorMsg -> getError로 변경
    2. getErrorMsg 추가

--- front/src/util/error.ts
+++ front/src/util/error.ts
@@ -1,5 +1,5 @@
 import { AxiosError } from "axios";
-import { isArray } from "lodash";
+import _, { isArray } from "lodash";
 
 interface FastAPIError {
   data: {
@@ -7,10 +7,18 @@ interface FastAPIError {
   };
 }
 
-export const getErrorMsg = (err: AxiosError) => {
+export const getError = (err: AxiosError) => {
   const response = err.response as FastAPIError;
 
   if (!response || !response.data) return "잠시 후 다시 시도해 주세요";
   if (isArray(!response.data.detail)) return response.data.detail[0].msg;
   return response.data.detail;
+};
+
+export const getErrorMsg = (err: AxiosError) => {
+  const error = getError(err);
+
+  if (_.isString(error)) return error;
+
+  return error[0].msg;
 };
