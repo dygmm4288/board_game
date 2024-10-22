016fae7d7bb53925b4134c0b23a24965cae195fb
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-22 21:57:27 +0900

    (add) useErrorModal

---
+++ front/src/hooks/useErrorModal.tsx
@@ -0,0 +1,22 @@
+import { AxiosError } from "axios";
+import AlertModal from "../components/common/modal/AlertModal";
+import { getErrorMsg } from "../util/error";
+import useModal from "../zustand/modal";
+
+type ShowErrorType = (error: AxiosError, title?: string) => void;
+
+const useErrorModal = () => {
+  const { show } = useModal();
+
+  const showError: ShowErrorType = (error, title = "실패") => {
+    show({
+      component: <AlertModal title={title} content={getErrorMsg(error)} />,
+    });
+  };
+
+  return {
+    showError,
+  };
+};
+
+export default useErrorModal;
