09e2d083ac5f0b42038fc54659a271143734b96c
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-22 22:05:51 +0900

    (update) useErrorMsg : error에 string도 받을 수 있게 수정

--- front/src/hooks/useErrorModal.tsx
+++ front/src/hooks/useErrorModal.tsx
@@ -3,14 +3,15 @@ import AlertModal from "../components/common/modal/AlertModal";
 import { getErrorMsg } from "../util/error";
 import useModal from "../zustand/modal";
 
-type ShowErrorType = (error: AxiosError, title?: string) => void;
+type ShowErrorType = (error: string | AxiosError, title?: string) => void;
 
 const useErrorModal = () => {
   const { show } = useModal();
 
   const showError: ShowErrorType = (error, title = "실패") => {
+    const content = error instanceof AxiosError ? getErrorMsg(error) : error;
     show({
-      component: <AlertModal title={title} content={getErrorMsg(error)} />,
+      component: <AlertModal title={title} content={content} />,
       isCloseClick: false,
     });
   };
