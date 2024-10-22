85cec2abf43cb08b060b15549c7c6aeb64734849
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-10-22 22:03:18 +0900

    (update) alert modal 닫기 버튼 추가

--- front/src/components/common/modal/AlertModal.tsx
+++ front/src/components/common/modal/AlertModal.tsx
@@ -1,12 +1,16 @@
+import useModal from "../../../zustand/modal";
+
 type Props = {
   title?: string;
   content?: string;
 };
 const AlertModal = ({ title, content }: Props) => {
+  const { hide } = useModal();
   return (
     <article className='px-[50px] py-[20px] w-[240px] flex flex-col gap-[20px] bg-white rounded-10'>
       {title && <h1>{title}</h1>}
       {content && <p>{content}</p>}
+      <button onClick={() => hide()}>닫기</button>
     </article>
   );
 };
